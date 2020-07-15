1cd78afddf698bb42d5095e803679c571494b687
Q118 <shelbyfish91@gmail.com>
2020-07-14 20:46:36 -0700

    pseudo coding rest of functions

--- pseudo.md
+++ pseudo.md
@@ -24,25 +24,71 @@ if time, add more like in gif
 - Add new department
   - Create addDepartment function /
   - prompt user for a department name /
-  - create query string to create department
-  - run connection.query
-  - if there is an error
-  - then print unable to process
-  - else print success message
-  - call start menu
+  - create query string to create department /
+  - run connection.query /
+  - if there is an error /
+  - then print unable to process /
+  - else print success message /
+  - call start menu /
+
 - View Departments
+  -create view dept funciton
+  -no prompt
+  -display the department table info
+    - ...
+  -back to start menu
+
 - Add new role
+  -create addrole function
+  -prompt what is the title
+  -prompt what is the salary
+  -prompt department id
+  - run connection.query 
+  - if there is an error 
+  - then print unable to process 
+  - else print success message 
+  - call start menu 
+
 - View Roles
+    -create view roles funciton
+    -no prompt
+    -display the roles table info
+      -...
+    -back to start menu
+
 - Add new Employee
+  -create add employee function
+  -prompt user for employe first name
+  -prompt user for employe last name
+  -prompt what is the employees role id
+  -promt user for the employyees mangr id (if applicable)
+      -option for n/a
+   - run connection.query 
+  - if there is an error 
+  - then print unable to process 
+  - else print success message 
+  - call start menu 
+
+
 - View employee
+    -create view employee funciton
+    -no prompt
+    -display the employee table info
+    -back to start menu
+
+
 - Update employee role
+  -create update employee function
+  -prompt choice of who to update
+  -display list of employees already in table
+  -prompt which part to update
+  -display it and they choose
+  -prompt want to change another part?
+  -if yes, then go up two steps, 
+  -if no, print employee updated,
+  -back to start menu
 
 
-
-//first make start prompt
-what would you like to do
-  
-  based on choice...
 
 
   functions
--- server.js
+++ server.js
@@ -37,61 +37,60 @@ function start() {
 			],
 		})
 		.then((answer) => {
-		// 	// based on their answer, call appropriate functions
-		 	let answer1 = answer.startApp;
-		 	if (answer1 === "Add new department") {
-		 		return addNewDept();
-		// 	} else if (answer1 === "View Departments") {
-		// 		return viewDept();
-		// 	} else if (answer1 === "Add new role") {
-		// 		return addRole();
-		// 	} else if (answer1 === "View Roles") {
-		// 		return viewRole();
-		// 	} else if (answer1 === "Add new Employee") {
-		// 		return addEmployee();
-		// 	} else if (answer1 === "View employee(s)") {
-		// 		return viewEmployee();
-		// 	} else if (answer1 === "Update employee role(s)") {
-		// 		return updateEmployeeRole();
-		 	} else {
-		 		connection.end();
-		 	}
-		 })
-		 .catch((error) => {
-		 	console.log(error);
+			// 	// based on their answer, call appropriate functions
+			let answer1 = answer.startApp;
+			if (answer1 === "Add new department") {
+				return addNewDept();
+				// 	} else if (answer1 === "View Departments") {
+				// 		return viewDept();
+				// 	} else if (answer1 === "Add new role") {
+				// 		return addRole();
+				// 	} else if (answer1 === "View Roles") {
+				// 		return viewRole();
+				// 	} else if (answer1 === "Add new Employee") {
+				// 		return addEmployee();
+				// 	} else if (answer1 === "View employee(s)") {
+				// 		return viewEmployee();
+				// 	} else if (answer1 === "Update employee role(s)") {
+				// 		return updateEmployeeRole();
+			} else {
+				connection.end();
+			}
+		})
+		.catch((error) => {
+			console.log(error);
 			process.exit(1);
 		});
 }
 
 function addNewDept() {
-  // prompt for info about department being added
-  return inquirer
-    .prompt([
-      {
-        name: "deptName",
-        type: "input",
-        message: "What is the name of the department?",
-      },
-    ])
-    .then((answer) => {
-      // when finished prompting, insert a new item into the db with that info
-      return connection.query(
-        "INSERT INTO department SET ?",
-        {
-          name: answer.deptName,
-        },
-        (err) => {
-          if (err) {
-            throw err;
-          }
-          console.log("Your department has been added successfully!");
-          // re-prompt the user for if they want to bid or post
-          return start();
-        }
-      );
-    });
+	// prompt for info about department being added
+	return inquirer
+		.prompt([
+			{
+				name: "deptName",
+				type: "input",
+				message: "What is the name of the department?",
+			},
+		])
+		.then((answer) => {
+			// when finished prompting, insert a new item into the db with that info
+			return connection.query(
+				"INSERT INTO department SET ?",
+				{
+					name: answer.deptName,
+				},
+				(err) => {
+					if (err) {
+						throw err;
+					}
+					console.log("Your department has been added successfully!");
+					// re-prompt the user for if they want to bid or post
+					return start();
+				}
+			);
+		});
 }
-  
 
 // viewDept();
 // addRole();
