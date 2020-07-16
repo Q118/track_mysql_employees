c8138d9a43d97f8885ce8adc8f77b6ec20c47166
Q118 <shelbyfish91@gmail.com>
2020-07-15 18:06:54 -0700

    adding in seed data

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Ford", 1, 3);
+
+INSERT INTO department(name) VALUES ("engineering");
+INSERT INTO department(name) VALUES ("clothing");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Creative Director", 100000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Design DIrector", 80000, 2);
--- server.js
+++ server.js
@@ -96,5 +96,57 @@ function addNewDept() {
 // addRole();
 // viewRole();
 // addEmployee();
-// viewEmployee();
-// updateEmployeeRole();
+// function viewEmployee() {
+// 	const sqlString = `sqlquery`; //no values below bc no ?s
+// 	connection.query(sqlString, (error, results) => {
+// 		if (error) {
+// 			throw error;
+// 		}
+// 		console.table(results);
+// 		//time to use console.table
+// 		return start();
+// 	});
+// }
+
+// function updateEmployeeRole() {
+// 	//get all the employees
+
+// 	const sqlString = `
+// 	students.id AS ID screenshot 
+// 	eeeee`;
+// 	//then prompt user to pick a student
+// 	connection.query(sqlString, (error, results) => {
+// 		if (error) {
+// 			throw error;
+// 		}
+// 		console.table(results);
+// 		inquirer
+// 			.prompt({
+// 				name: "employeeID",
+// 				type: "input",
+// 				message: "Enter emplowee id to changer",
+// 			})
+// 			.then((studentChoiceAnswers) => {
+// 				//so make name = each row and value = the id
+// 				connection.query("SELECT * FROM classes;", (error, results) => {
+// 					console.table(results);
+// 					inqurier.prompt({
+// 						name: "classID",
+// 						type: "input",
+// 						message: "Enter emplowee id to changer",
+// 					}).then((classChoiceANswers) => {
+// 						cinst student ID= stendentchoiceansers.studentID
+// 						connection.query("UPDATE students SET classID = ? WHERE =?;", [studentID, classID])
+// 					});
+// 				});
+// 			});
+// 	});
+
+// 	//get all the classes
+// 	//then prompt user to pick the new class
+// 	//the up-date the employee
+// 	//then back to main
+// }
+
+// \;
+//////
